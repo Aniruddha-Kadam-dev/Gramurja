c37a2e00b5fe898384e57d2dc309ada39732d816
Aniruddha Kadam <aniruddhadroit@gmail.com>
2024-07-19 05:19:57 +0530

    Workpage - added projects
    since i missed the projects part from the old site, i added an inner accordion to represent the projects in a two column grid manner, even though the desktop version has some issues while opening the project accordion

--- work.js
+++ work.js
@@ -1,13 +1,28 @@
 const programs = document.querySelectorAll(".ps");
 programs.forEach((item) => {
   const block = item.querySelector(".p-block");
-  const content = item.querySelector(".p-content");
-  const block_p = item.querySelector(".p-des");
 
   block.addEventListener("click", () => {
     for (var i = 0; i < programs.length; i++) {
       if (programs[i] != item) {
         programs[i].classList.remove("open");
+      } else {
+        item.classList.toggle("open");
+      }
+    }
+  });
+});
+
+const projects = document.querySelectorAll(".proj");
+projects.forEach((item) => {
+  const proj_block = item.querySelector(".proj-block");
+  const proj_content = item.querySelector(".proj-content");
+  const block_p = item.querySelector(".proj-des");
+
+  proj_block.addEventListener("click", () => {
+    for (var i = 0; i < projects.length; i++) {
+      if (projects[i] != item) {
+        projects[i].classList.remove("open");
       } else {
         item.classList.toggle("open");
       }
